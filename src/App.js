955011b0b63c2391b2179e225fc8c2ed313ad789
Ricky Nugroho <rocanowi@yahoo.co.id>
2018-09-12 17:06:37 +0700

    Create static version of website

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -20,8 +20,159 @@ class App extends Component {
 
   renderActiveMeetup() {
     return (
-      <div className="active-meetup">
-        asdf
+      <div className="active-meetup grey-section">
+        <div className="photo-active-meetup">
+          <img src="img/grey.jpg" />
+        </div>
+        <div className="description-active-meetup">
+          <h3>Hacktive8 Meetup</h3>
+          <div className="description-active-meetup-content">
+            <div className="description-active-meetup-left">
+              <div className="left-content">Location</div>
+              <div className="right-content">Jakarta, Indonesia</div>
+            </div>
+            <div className="description-active-meetup-left">
+              <div className="left-content">Members</div>
+              <div className="right-content">1,078</div>
+            </div>
+            <div className="description-active-meetup-left">
+              <div className="left-content">Organizers</div>
+              <div className="right-content">Adhy Wiranata</div>
+            </div>
+            <div className="description-active-meetup-left">
+              <div>
+                <a href="" class="btn">Join Us</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  renderNextMeetup() {
+    return (
+      <div className="container-section">
+        <h3>Next Meetup</h3>
+        <div className="next-meetup grey-section">
+          <div className="next-meetup-title">
+            Awesome meetup and event
+          </div>
+          <div className="next-meetup-date">
+            25 January 2018
+          </div>
+          <div class="next-meetup-p">
+            <p>
+              <div>Hello, JavaScript & Node.js Ninjas!</div>
+              <div>Get ready for our monthly meetup JakartaJS! This will be our fifth meetup of 2018!</div>
+              <div>The meetup format will contain some short stories and technical talks.</div>
+              <div>If you have short announcement you'd like to share with the audience, you may do so during open mic accouncement.</div>
+            </p>
+            <p>
+              Remember to bring a photo ID to get through building security.
+            </p>
+            <p>
+              -----
+            </p>
+            <p>
+              See you there!
+            </p>
+            <p>
+              Best, Hengki, Giovanni, Sofian, Riza, Agung The JakartaJS Organizers
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  renderAboutMeetup() {
+    return (
+      <div className="about-meetup white-section">
+        <h3>About Meetup</h3>
+        <p>
+          Come and meet other developers.
+        </p>
+        <p>
+          Twitter: @JakartaJS.
+        </p>
+      </div>
+    );
+  }
+
+  renderMembers() {
+    return (
+      <div className="container-section">
+        <div class="container-section-flex">
+          <div>
+            <h3>Members</h3>
+          </div>
+          <div>
+            <a href="">See all</a>
+          </div>
+        </div>
+        <div className="member-list grey-section">
+          {this.renderMember()}
+          {this.renderMemberOther()}
+        </div>
+      </div>
+    );
+  }
+
+  renderMember() {
+    return (
+      <div className="member-section">
+        <div className="photo-member">
+          <img src="img/grey.jpg" />
+        </div>
+        <div className="profile-member">
+          <div>Organizers</div>
+          <div>Adhy Wiranata</div>
+        </div>
+      </div>
+    );
+  }
+
+  renderMemberOther() {
+    return (
+      <div className="member-section">
+        <a href="">4 others.</a>
+      </div>
+    );
+  }
+
+  renderPastMeetup() {
+    return (
+      <div className="past-meetup white-section">
+        <div class="container-section-flex">
+          <div>
+            <h3>Past Meetup</h3>
+          </div>
+          <div>
+            <a href="">See all</a>
+          </div>
+        </div>
+        <div class="past-meetup-list">
+          {this.renderMeetupEvent()}
+          {this.renderMeetupEvent()}
+          {this.renderMeetupEvent()}
+        </div>
+      </div>
+    );
+  }
+
+  renderMeetupEvent() {
+    return (
+      <div className="meetup-box">
+        27 November 2017
+        <hr />
+        #39 JakartaJS Meetup
+        <div class="meetup-went-total">
+          139 went
+        </div>
+        <div class="meetup-view-btn">
+          <a href="" class="btn-grey">View</a>
+        </div>
       </div>
     );
   }
@@ -31,7 +182,10 @@ class App extends Component {
       <div>
         {this.renderHeader()}
         {this.renderActiveMeetup()}
-        <h1>Halo, Hacktiv8!</h1>
+        {this.renderNextMeetup()}
+        {this.renderAboutMeetup()}
+        {this.renderMembers()}
+        {this.renderPastMeetup()}
       </div>
     );
   }
