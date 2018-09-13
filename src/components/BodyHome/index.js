5a3fabe284d796410f75a4cfb2d62baf22304ea6
Ricky Nugroho <rocanowi@yahoo.co.id>
2018-09-13 13:22:09 +0700

    Create body page component

---
+++ src/components/BodyHome/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class BodyHome extends Component {
+    render() {
+        return (
+            <div>
+                {/* {this.renderActiveMeetup()}
+                {this.renderNextMeetup()}
+                {this.renderAboutMeetup()}
+                {this.renderMembers()}
+                {this.renderPastMeetup()} */}
+            </div>
+        );
+    }
+}
