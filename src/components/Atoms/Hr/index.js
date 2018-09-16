76245c86c8c363f8107246184f1976e98caac64e
Ricky Nugroho <rocanowi@yahoo.co.id>
2018-09-17 00:18:12 +0700

    Add hr atom

---
+++ src/components/Atoms/Hr/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class Hr extends Component {
+    render() {
+        return (
+            <hr className={this.props.className} />
+        )
+    }
+}
