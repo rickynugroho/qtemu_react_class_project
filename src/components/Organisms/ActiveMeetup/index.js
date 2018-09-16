eafe4020571d8228f125f06a918573377226b3ba
Ricky Nugroho <rocanowi@yahoo.co.id>
2018-09-16 18:30:13 +0700

    Move some css to organism active meetup

--- src/components/Organisms/ActiveMeetup/index.js
+++ src/components/Organisms/ActiveMeetup/index.js
@@ -6,34 +6,37 @@ import TitleH3 from '../../Atoms/TitleH3';
 import LeftRightDescription from '../../Molecules/LeftRightDescription';
 import DivText from '../../Atoms/DivText';
 import Link from '../../Atoms/Link';
+import GreySection from '../../Molecules/GreySection';
 
 import './style.css';
 
 export default class ActiveMeetup extends Component {
     render() {
         return (
-            <DivText className="active-meetup grey-section">
-                <Image className="photo-active-meetup" src="img/grey.jpg" alt="grey area" />
-                <DivText className="description-active-meetup">
-                    <TitleH3>{this.props.infoMeetup.meetupName}</TitleH3>
-                    <DivText className="description-active-meetup-content">
-                        <LeftRightDescription>
-                            <DivText className="left-content">Location</DivText>
-                            <DivText className="right-content">{this.props.infoMeetup.location}</DivText>
-                        </LeftRightDescription>
-                        <LeftRightDescription>
-                            <DivText className="left-content">Members</DivText>
-                            <DivText className="right-content">{this.props.infoMeetup.numberOfMembers}</DivText>
-                        </LeftRightDescription>
-                        <LeftRightDescription>
-                            <DivText className="left-content">Organizers</DivText>
-                            <DivText className="right-content">{this.props.infoMeetup.headOrganizer}</DivText>
-                        </LeftRightDescription>
-                        <LeftRightDescription>
-                            <Link className="btn" text="Join Us"/>
-                        </LeftRightDescription>
+            <DivText className="active-meetup">
+                <GreySection className=" grey-section">
+                    <Image className="photo-active-meetup" src="img/grey.jpg" alt="grey area" />
+                    <DivText className="description-active-meetup">
+                        <TitleH3>{this.props.infoMeetup.meetupName}</TitleH3>
+                        <DivText className="description-active-meetup-content">
+                            <LeftRightDescription>
+                                <DivText className="left-content">Location</DivText>
+                                <DivText className="right-content">{this.props.infoMeetup.location}</DivText>
+                            </LeftRightDescription>
+                            <LeftRightDescription>
+                                <DivText className="left-content">Members</DivText>
+                                <DivText className="right-content">{this.props.infoMeetup.numberOfMembers}</DivText>
+                            </LeftRightDescription>
+                            <LeftRightDescription>
+                                <DivText className="left-content">Organizers</DivText>
+                                <DivText className="right-content">{this.props.infoMeetup.headOrganizer}</DivText>
+                            </LeftRightDescription>
+                            <LeftRightDescription>
+                                <Link className="btn" text="Join Us"/>
+                            </LeftRightDescription>
+                        </DivText>
                     </DivText>
-                </DivText>
+                </GreySection>
             </DivText>
         );
     }
