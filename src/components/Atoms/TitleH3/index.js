a0a77d5917e3ddc9b307685fb7e3d18c629ab952
Ricky Nugroho <rocanowi@yahoo.co.id>
2018-09-16 18:20:41 +0700

    Atom titleh3 now use children

--- src/components/Atoms/TitleH3/index.js
+++ src/components/Atoms/TitleH3/index.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class Image extends Component {
     render() {
         return (
-            <h3 className={this.props.className}>{this.props.text}</h3>
+            <h3 className={this.props.className}>{this.props.children}</h3>
         )
     }
 }
