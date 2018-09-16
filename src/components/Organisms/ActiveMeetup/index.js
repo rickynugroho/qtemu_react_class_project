cbe69cf4a89dccfefdf792c49fd36dd807ff5410
Ricky Nugroho <rocanowi@yahoo.co.id>
2018-09-16 17:19:16 +0700

    Refactor active meetup organism to atomic design

--- src/components/Organisms/ActiveMeetup/index.js
+++ src/components/Organisms/ActiveMeetup/index.js
@@ -3,6 +3,9 @@ import React, { Component } from 'react';
 
 import Image from '../../Atoms/Image';
 import TitleH3 from '../../Atoms/TitleH3';
+import LeftRightDescription from '../../Molecules/LeftRightDescription';
+import DivText from '../../Atoms/DivText';
+import Link from '../../Atoms/Link';
 
 import './style.css';
 
@@ -12,25 +15,23 @@ export default class ActiveMeetup extends Component {
             <div className="active-meetup grey-section">
                 <Image className="photo-active-meetup" src="img/grey.jpg" alt="grey area" />
                 <div className="description-active-meetup">
-                    <TitleH3 text="Hacktive8 Meetup" />
+                    <TitleH3 text={this.props.infoMeetup.meetupName} />
                     <div className="description-active-meetup-content">
-                        <div className="description-active-meetup-left">
-                            <div className="left-content">Location</div>
-                            <div className="right-content">Jakarta, Indonesia</div>
-                        </div>
-                        <div className="description-active-meetup-left">
-                            <div className="left-content">Members</div>
-                            <div className="right-content">1,078</div>
-                        </div>
-                        <div className="description-active-meetup-left">
-                            <div className="left-content">Organizers</div>
-                            <div className="right-content">Adhy Wiranata</div>
-                        </div>
-                        <div className="description-active-meetup-left">
-                            <div>
-                                <a href="" className="btn">Join Us</a>
-                            </div>
-                        </div>
+                        <LeftRightDescription>
+                            <DivText className="left-content" text="Location" />
+                            <DivText className="right-content" text={this.props.infoMeetup.location} />
+                        </LeftRightDescription>
+                        <LeftRightDescription>
+                            <DivText className="left-content" text="Members" />
+                            <DivText className="right-content" text={this.props.infoMeetup.numberOfMembers} />
+                        </LeftRightDescription>
+                        <LeftRightDescription>
+                            <DivText className="left-content" text="Organizers" />
+                            <DivText className="right-content" text={this.props.infoMeetup.headOrganizer} />
+                        </LeftRightDescription>
+                        <LeftRightDescription>
+                            <Link className="btn" text="Join Us"/>
+                        </LeftRightDescription>
                     </div>
                 </div>
             </div>
