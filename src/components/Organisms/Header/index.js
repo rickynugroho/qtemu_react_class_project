7d311fe9e2a2a0dbb92e9277dc38f2f562337d0c
Ricky Nugroho <rocanowi@yahoo.co.id>
2018-09-21 00:11:07 +0700

    Styling app bar

--- src/components/Organisms/Header/index.js
+++ src/components/Organisms/Header/index.js
@@ -5,12 +5,14 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import { Grid } from '@material-ui/core';
 import IconButton from '@material-ui/core/IconButton';
 // import MenuIcon from '@material-ui/icons/Menu';
 import { withStyles } from '@material-ui/core/styles';
 // import HeaderLink from '../../Molecules/HeaderLink';
 // import Link from '../../Atoms/Link';
 
+import 'typeface-roboto'
 import './style.css';
 
 const styles = {
@@ -22,9 +24,6 @@ const styles = {
     },
     menuButton: {
         marginLeft: -12,
-        marginRight: 20,
-    },
-    title: {
         marginRight: 20,
     },
 };
@@ -56,11 +55,44 @@ class Header extends Component {
                             <MenuIcon />
                         </IconButton> 
                         */}
-                        <Typography className={styles.title} variant="title" color="inherit">
-                            Qtemu
-                        </Typography>
-                        <Button color="inherit">Create Meetup</Button>
-                        <Button color="inherit">Explore</Button>
+                        <Grid
+                            container
+                            direction="row"
+                            justify="space-between"
+                            alignItems="center"
+                            container spacing={12}
+                        >
+                            <Grid item xs={6}>
+                                <Grid
+                                    container
+                                    direction="row"
+                                    justify="flex-start"
+                                    alignItems="center"
+                                    spacing={12}
+                                >
+                                    <Grid item xs={3} xl={2}>
+                                        <Typography className={styles.grow} variant="title" color="inherit">
+                                            Qtemu
+                                        </Typography>
+                                    </Grid>
+                                    <Grid item xs={3} xl={2}>
+                                        <Button color="inherit">Create Meetup</Button>
+                                    </Grid>
+                                    <Grid item xs={3} xl={2}>
+                                        <Button color="inherit">Explore</Button>
+                                    </Grid>
+                                </Grid>
+                            </Grid>
+                            <Grid item xs={6}>
+                                <Grid
+                                    container
+                                    direction="row"
+                                    justify="flex-end"
+                                >
+                                    <Button color="inherit">Login</Button>
+                                </Grid>
+                            </Grid>
+                        </Grid>
                     </Toolbar>
                 </AppBar>
             </div>
